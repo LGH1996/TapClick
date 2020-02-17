4406ef16eb46f710911a67946c6b9f871fd3505d
LGH1996 <2281442260@qq.com>
2020-02-18 00:35:23 +0800

    -

--- app/src/main/java/com/lgh/advertising/myactivity/AppAuthorizationActivity.java
+++ app/src/main/java/com/lgh/advertising/myactivity/AppAuthorizationActivity.java
@@ -34,71 +34,66 @@ public class AppAuthorizationActivity extends Activity {
         appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
         packageManager = getPackageManager();
         RelativeLayout accessibilityOnOff = findViewById(R.id.accessibility_on_off);
+        RelativeLayout batteryIgnoreOnOff = findViewById(R.id.batteryIgnore_on_off);
+        RelativeLayout alertWindowOnOff = findViewById(R.id.alertWindow_on_off);
         RelativeLayout usageStatsOnOff = findViewById(R.id.usageStats_on_off);
-        RelativeLayout batteryOnOff = findViewById(R.id.batteryIgnore_on_off);
-        RelativeLayout alertWindowOnOff = findViewById(R.id.alert_window_off);
         ImageView.OnClickListener onOffClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.accessibility_on_off:
-                        Intent intentA = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-                        startActivity(intentA);
-                        break;
-                    case R.id.usageStats_on_off:
-                        Intent intentB = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
-                        if (intentB.resolveActivity(packageManager) != null) {
-                            startActivity(intentB);
-                        } else {
-                            Toast.makeText(context, "授权窗口打开失败,请手动打开", Toast.LENGTH_SHORT).show();
-                        }
+                        Intent intentAccessibility = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                        startActivity(intentAccessibility);
                         break;
                     case R.id.batteryIgnore_on_off:
                         if (((PowerManager) getSystemService(POWER_SERVICE)).isIgnoringBatteryOptimizations(getPackageName())) {
                             Toast.makeText(context, "忽略电池优化权限已打开", Toast.LENGTH_SHORT).show();
                         }else {
-                            Intent intentC = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + getPackageName()));
-                            if (intentC.resolveActivity(packageManager) != null) {
-                                startActivity(intentC);
+                            Intent intentBatteryIgnore = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + getPackageName()));
+                            if (intentBatteryIgnore.resolveActivity(packageManager) != null) {
+                                startActivity(intentBatteryIgnore);
                             } else {
                                 Toast.makeText(context, "授权窗口打开失败，请手动打开", Toast.LENGTH_SHORT).show();
                             }
                         }
                         break;
-                    case R.id.alert_window_off:
-                        Intent intentD = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-                        if (intentD.resolveActivity(packageManager) != null) {
-                            startActivity(intentD);
+                    case R.id.alertWindow_on_off:
+                        Intent intentAlertWindow = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                        if (intentAlertWindow.resolveActivity(packageManager) != null) {
+                            startActivity(intentAlertWindow);
                         } else {
                             Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
                             startActivity(intent);
+                        }
+                        break;
+                    case R.id.usageStats_on_off:
+                        Intent intentUsageStats = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+                        if (intentUsageStats.resolveActivity(packageManager) != null) {
+                            startActivity(intentUsageStats);
+                        } else {
+                            Toast.makeText(context, "授权窗口打开失败,请手动打开", Toast.LENGTH_SHORT).show();
                         }
                         break;
                 }
             }
         };
         accessibilityOnOff.setOnClickListener(onOffClickListener);
-        usageStatsOnOff.setOnClickListener(onOffClickListener);
-        batteryOnOff.setOnClickListener(onOffClickListener);
+        batteryIgnoreOnOff.setOnClickListener(onOffClickListener);
         alertWindowOnOff.setOnClickListener(onOffClickListener);
+        usageStatsOnOff.setOnClickListener(onOffClickListener);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         ImageView accessibilityOnOffImg = findViewById(R.id.accessibility_on_off_img);
-        ImageView usageStatsImg = findViewById(R.id.usageStats_on_off_img);
         ImageView batteryOnOffImg = findViewById(R.id.batteryIgnore_on_off_img);
         ImageView alertWindowOnOffImg = findViewById(R.id.alert_window_on_off_img);
+        ImageView usageStatsImg = findViewById(R.id.usageStats_on_off_img);
         if (MyAccessibilityService.mainFunction == null && MyAccessibilityServiceNoGesture.mainFunction == null) {
             accessibilityOnOffImg.setImageResource(R.drawable.error);
         } else {
             accessibilityOnOffImg.setImageResource(R.drawable.ok);
-        }
-        if (appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), getPackageName()) == AppOpsManager.MODE_ALLOWED){
-            usageStatsImg.setImageResource(R.drawable.ok);
-        } else {
-            usageStatsImg.setImageResource(R.drawable.error);
         }
         if (((PowerManager) getSystemService(POWER_SERVICE)).isIgnoringBatteryOptimizations(getPackageName())) {
             batteryOnOffImg.setImageResource(R.drawable.ok);
@@ -109,6 +104,11 @@ public class AppAuthorizationActivity extends Activity {
             alertWindowOnOffImg.setImageResource(R.drawable.ok);
         } else {
             alertWindowOnOffImg.setImageResource(R.drawable.error);
+        }
+        if (appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), getPackageName()) == AppOpsManager.MODE_ALLOWED){
+            usageStatsImg.setImageResource(R.drawable.ok);
+        } else {
+            usageStatsImg.setImageResource(R.drawable.error);
         }
     }
 }
