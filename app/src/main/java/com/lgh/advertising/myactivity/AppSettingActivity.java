11197ebf765657f452b484738ccadc6bca0eb078
LGH1996 <2281442260@qq.com>
2020-03-05 21:52:04 +0800

    -

--- app/src/main/java/com/lgh/advertising/myactivity/AppSettingActivity.java
+++ app/src/main/java/com/lgh/advertising/myactivity/AppSettingActivity.java
@@ -9,20 +9,15 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.provider.Settings;
 import android.text.Html;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,18 +28,11 @@ import com.lgh.advertising.myclass.LatestMessage;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import okhttp3.Headers;
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class AppSettingActivity extends Activity {
@@ -64,7 +52,7 @@ public class AppSettingActivity extends Activity {
         setResult(Activity.RESULT_OK, intent);
         Button openDetail = findViewById(R.id.setting_open);
         Button checkUpdate = findViewById(R.id.setting_update);
-        Button submitDebug = findViewById(R.id.setting_submit);
+        TextView authorChat = findViewById(R.id.authorChat);
         CheckBox checkBox = findViewById(R.id.setting_autoHideOnTaskList);
         checkBox.setChecked(autoHideOnTaskList);
         openDetail.setOnClickListener(new View.OnClickListener() {
@@ -151,99 +139,12 @@ public class AppSettingActivity extends Activity {
                 asyncTask.execute("https://api.github.com/repos/LGH1996/ADGORELEASE/releases/latest");
             }
         });
-        submitDebug.setOnClickListener(new View.OnClickListener() {
+        authorChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    final LayoutInflater inflater = LayoutInflater.from(context);
-                    final View view = inflater.inflate(R.layout.view_commit, null);
-                    final AlertDialog dialogCommit = new AlertDialog.Builder(AppSettingActivity.this).setView(view).setCancelable(false).create();
-                    final EditText textView = view.findViewById(R.id.editText);
-                    TextView but_empty = view.findViewById(R.id.empty);
-                    TextView but_cancel = view.findViewById(R.id.cancel);
-                    TextView but_sure = view.findViewById(R.id.sure);
-                    but_empty.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            textView.setText("");
-                        }
-                    });
-                    but_cancel.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            dialogCommit.dismiss();
-                        }
-                    });
-                    but_sure.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            try {
-                                final String commitMessage = textView.getText().toString().trim();
-                                if (commitMessage.isEmpty()) {
-                                    textView.setText("");
-                                    Toast.makeText(context, "内容不能为空", Toast.LENGTH_SHORT).show();
-                                    return;
-                                }
-                                @SuppressLint("StaticFieldLeak") AsyncTask<String, Integer, String> asyncTask = new AsyncTask<String, Integer, String>() {
-                                    private boolean successful;
-                                    private AlertDialog waitDialog;
-
-                                    @Override
-                                    protected void onPreExecute() {
-                                        super.onPreExecute();
-                                        successful = true;
-                                        waitDialog = new AlertDialog.Builder(AppSettingActivity.this).setView(new ProgressBar(context)).setCancelable(false).create();
-                                        Window window = waitDialog.getWindow();
-                                        if (window != null)
-                                            window.setBackgroundDrawableResource(R.color.transparent);
-                                        waitDialog.show();
-
-                                    }
-
-                                    @Override
-                                    protected String doInBackground(String... strings) {
-                                        try {
-                                            HashMap<String, String> postMessage = new HashMap<>();
-                                            postMessage.put("message", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss a", Locale.ENGLISH).format(new Date()));
-                                            postMessage.put("content", Base64.encodeToString(commitMessage.getBytes(), Base64.DEFAULT));
-                                            RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), new Gson().toJson(postMessage));
-                                            Headers headers = new Headers.Builder().add("Authorization", "token b487ac9fc2cd667854db7bf81ee8c36e23ca3bb3").build();
-                                            Request request = new Request.Builder().put(requestBody).headers(headers).url(strings[0]).build();
-                                            OkHttpClient httpClient = new OkHttpClient();
-                                            Response response = httpClient.newCall(request).execute();
-                                            response.close();
-                                        } catch (Throwable e) {
-                                            successful = false;
-                                            e.printStackTrace();
-                                        }
-                                        return null;
-                                    }
-
-                                    @Override
-                                    protected void onPostExecute(String s) {
-                                        super.onPostExecute(s);
-                                        waitDialog.dismiss();
-                                        if (successful) {
-                                            Toast.makeText(context, "提交成功", Toast.LENGTH_SHORT).show();
-                                            dialogCommit.dismiss();
-                                        } else {
-                                            Toast.makeText(context, "提交失败", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }
-                                };
-                                asyncTask.execute("https://api.github.com/repos/LGH1996/ADGORELEASE/contents/" + Build.PRODUCT + "_" + Build.VERSION.SDK_INT + "(" + Build.VERSION.RELEASE + ")" + "_" + SystemClock.uptimeMillis() + ".doc");
-                            } catch (Throwable e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-                    Window win = dialogCommit.getWindow();
-                    win.setBackgroundDrawableResource(R.color.dialogBackground);
-                    win.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
-                    win.setDimAmount(0);
-                    dialogCommit.show();
-                } catch (Throwable e) {
-                    e.printStackTrace();
+                Intent openChat = new Intent(Intent.ACTION_VIEW, Uri.parse("mqqwpa://im/chat?chat_type=wpa&uin=2281442260"));
+                if (openChat.resolveActivity(getPackageManager()) != null) {
+                    startActivity(openChat);
                 }
             }
         });
