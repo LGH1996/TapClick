c60830c03e72d11923cb9d08739989579967a759
lingh <2281442260@qq.com>
2022-12-19 22:28:36 +0800

    v1.12 新增设备管理器权限，提高保活率

--- app/src/main/java/com/lgh/advertising/going/myactivity/AuthorizationActivity.java
+++ app/src/main/java/com/lgh/advertising/going/myactivity/AuthorizationActivity.java
@@ -1,5 +1,8 @@
 package com.lgh.advertising.going.myactivity;
 
+import android.annotation.SuppressLint;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,6 +17,7 @@ import android.widget.Toast;
 import com.lgh.advertising.going.R;
 import com.lgh.advertising.going.databinding.ActivityAuthorizationBinding;
 import com.lgh.advertising.going.myclass.MyApplication;
+import com.lgh.advertising.going.myfunction.MyDeviceAdminReceiver;
 import com.lgh.advertising.going.myfunction.MyUtils;
 
 public class AuthorizationActivity extends BaseActivity {
@@ -23,6 +27,8 @@ public class AuthorizationActivity extends BaseActivity {
     private MyUtils myUtils;
     private Handler handler;
     private PackageManager packageManager;
+    private DevicePolicyManager devicePolicyManager;
+    private PowerManager powerManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +39,40 @@ public class AuthorizationActivity extends BaseActivity {
         myUtils = MyApplication.myUtils;
         handler = new Handler();
         packageManager = getPackageManager();
+        devicePolicyManager = getSystemService(DevicePolicyManager.class);
+        powerManager = getSystemService(PowerManager.class);
+
 
         View.OnClickListener onOffClickListener = new View.OnClickListener() {
+            @SuppressLint("NonConstantResourceId")
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.accessibility_on_off:
                         Intent intentAccessibility = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-                        startActivity(intentAccessibility);
+                        if (intentAccessibility.resolveActivity(packageManager) != null) {
+                            startActivity(intentAccessibility);
+                        } else {
+                            Toast.makeText(context, "授权窗口打开失败，请手动打开", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case R.id.device_admin_on_off:
+                        ComponentName compMyDeviceAdmin = new ComponentName(context, MyDeviceAdminReceiver.class);
+                        if (devicePolicyManager.isAdminActive(compMyDeviceAdmin)) {
+                            Toast.makeText(context, "设备管理器权限已开启", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Intent intentDeviceAdmin = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                            intentDeviceAdmin.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compMyDeviceAdmin);
+                            if (intentDeviceAdmin.resolveActivity(packageManager) != null) {
+                                startActivity(intentDeviceAdmin);
+                            } else {
+                                Toast.makeText(context, "授权窗口打开失败，请手动打开", Toast.LENGTH_SHORT).show();
+                            }
+                        }
                         break;
                     case R.id.batteryIgnore_on_off:
-                        if (getSystemService(PowerManager.class).isIgnoringBatteryOptimizations(getPackageName())) {
-                            Toast.makeText(context, "忽略电池优化权限已打开", Toast.LENGTH_SHORT).show();
+                        if (powerManager.isIgnoringBatteryOptimizations(getPackageName())) {
+                            Toast.makeText(context, "忽略电池优化权限已开启", Toast.LENGTH_SHORT).show();
                         } else {
                             Intent intentBatteryIgnore = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + getPackageName()));
                             if (intentBatteryIgnore.resolveActivity(packageManager) != null) {
@@ -58,6 +86,7 @@ public class AuthorizationActivity extends BaseActivity {
             }
         };
         authorizationBinding.accessibilityOnOff.setOnClickListener(onOffClickListener);
+        authorizationBinding.deviceAdminOnOff.setOnClickListener(onOffClickListener);
         authorizationBinding.batteryIgnoreOnOff.setOnClickListener(onOffClickListener);
     }
 
@@ -72,7 +101,12 @@ public class AuthorizationActivity extends BaseActivity {
                 } else {
                     authorizationBinding.accessibilityOnOffImg.setImageResource(R.drawable.ic_error);
                 }
-                if (getSystemService(PowerManager.class).isIgnoringBatteryOptimizations(getPackageName())) {
+                if (devicePolicyManager.isAdminActive(new ComponentName(context, MyDeviceAdminReceiver.class))) {
+                    authorizationBinding.deviceAdminOnOffImg.setImageResource(R.drawable.ic_ok);
+                } else {
+                    authorizationBinding.deviceAdminOnOffImg.setImageResource(R.drawable.ic_error);
+                }
+                if (powerManager.isIgnoringBatteryOptimizations(getPackageName())) {
                     authorizationBinding.batteryIgnoreOnOffImg.setImageResource(R.drawable.ic_ok);
                 } else {
                     authorizationBinding.batteryIgnoreOnOffImg.setImageResource(R.drawable.ic_error);
---
+++ app/src/main/java/com/lgh/advertising/going/myfunction/MyDeviceAdminReceiver.java
@@ -0,0 +1,13 @@
+package com.lgh.advertising.going.myfunction;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
